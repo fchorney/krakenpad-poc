338e973d8b1a5d82c26b35d731976513853f4b26
Fernando Chorney <github@djsbx.com>
2026-07-09 20:28:14 -0500

    Remove CMake build output from tracking; add .gitignore

--- firmware/panel/c/build/_deps/picotool-build/data_locs.cpp
+++
@@ -1,7 +0,0 @@
-
-#include <vector>
-#include <string>
-
-std::vector<std::string> data_locs = {
-    "./","/Users/fchorney/Documents/repos/personal/claude-smx-custom-hardware/firmware/panel/c/build/_deps/picotool/"
-};
